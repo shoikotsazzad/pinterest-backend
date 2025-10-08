59cb55d6b621c2282b7782cca2abcf0242f8a4c7
shoikotsazzad <mdshoikot202123@gmail.com>
2025-10-08 17:40:18 +0600

    profilePage and UserInformation Data fetching Done

--- controllers/pin.controller.js
+++ controllers/pin.controller.js
@@ -1,18 +1,39 @@
 import Pin from "../models/pin.model.js";
+import User from "../models/user.model.js";
 
 export const getPins = async (req, res) => {
+  const pageNumber = Number(req.query.cursor) || 0;
+  const search = req.query.search;
+  const userId = req.query.userId;
+  const LIMIT = 500;
 
-    const pageNumber = Number(req.query.cursor) || 0;
-    const LIMIT = 21;
-    const pins = await Pin.find()
-        .limit(LIMIT)
-        .skip(pageNumber * LIMIT);
+  const pins = await Pin.find(search ?{
+    $or: [
+        {title: { $regex: search, $options: 'i' }},
+        {tags: { $in: [search]}},
+    ],
+  } :userId 
+  ? {user: userId}
+  : {}
+)
+    .sort({ createdAt: -1 })
+    .skip(pageNumber * LIMIT)
+    .limit(LIMIT);
 
-    const hasNextPage = pins.length === LIMIT;
+  const totalPins = await Pin.countDocuments();
+  const hasNextPage = (pageNumber + 1) * LIMIT < totalPins;
 
-    await new Promise((resolve) => setTimeout(resolve, 3000));
+//   await new Promise((resolve) => setTimeout(resolve, 3000));
 
-    res
-        .status(200)
-        .json({ pins, nextCursor: hasNextPage ? pageNumber + 1 : null });
+  res.status(200).json({
+    pins,
+    nextCursor: hasNextPage ? pageNumber + 1 : null,
+  });
+};
+
+export const getPin = async (req, res) => {
+    const {id} = req.params;
+    const pin = await Pin.findById(id).populate("user", "username img displayName");
+
+    res.status(200).json(pin);
 }
